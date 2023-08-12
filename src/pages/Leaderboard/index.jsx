324c2c1fd5d039f6b329d479e1ec01a1bfd09138
burhan333 <em.burhan@yahoo.com>
2023-08-12 18:19:52 +0500

    leaderboard ui

---
+++ src/pages/Leaderboard/index.jsx
@@ -0,0 +1,80 @@
+const Leaderboard = () => {
+    return(
+        <div className="leader">
+            <p className="leader_title">LEADERBOARD</p>
+            <div className="leader_body">
+                <div className="leader_colum">
+                    <p className="leader_subTitle">Easy</p>
+                    <div className="leader_head">
+                        <p>Name</p>
+                        <p>Score</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                </div>
+                <div className="leader_colum">
+                    <p className="leader_subTitle">Medium</p>
+                    <div className="leader_head">
+                        <p>Name</p>
+                        <p>Score</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                </div>
+                <div className="leader_colum">
+                    <p className="leader_subTitle">Hard</p>
+                    <div className="leader_head">
+                        <p>Name</p>
+                        <p>Score</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                    <div className="leader_row">
+                        <p>Burhan</p>
+                        <p>80</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Leaderboard
